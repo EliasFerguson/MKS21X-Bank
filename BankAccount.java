1ccab8272825445c660bf58482a0df55eafcf05b
Elias Ferguson <eferguson00@stuy.edu>
2018-10-03 17:05:04 -0400

    added functional methods

--- BankAccount.java
+++ BankAccount.java
@@ -19,4 +19,21 @@ public class BankAccount {
   public int getAccountID() {
     return accountID;
   }
+  public void setPassword(String newPass) {
+    password = newPass;
+  }
+  public boolean deposit(double amount) {
+    if (amount > 0) {
+      balance += amount;
+      return true;
+    }
+    return false;
+  }
+  public boolean withdraw(double amount) {
+    if (amount > 0 && amount <= balance) {
+      balance -= amount;
+      return true;
+    }
+    return false;
+  }
 }
