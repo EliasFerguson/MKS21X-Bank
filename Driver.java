7859f8dbd78ad2bd6f65de0e6e6c75706b27fbf2
Elias Ferguson <eferguson00@stuy.edu>
2018-10-03 20:31:54 -0400

    successfully created a test for depositing

--- Driver.java
+++ Driver.java
@@ -2,5 +2,13 @@ public class Driver {
   public static void main(String[]args) {
     BankAccount acc = new BankAccount(1000000, 0000, "password");
     System.out.println(acc.toString());
+    acc.setPassword("newpass");
+    if (acc.deposit(400)) {
+      System.out.println("Successfully Deposited.");
+    }
+    else {
+      System.out.println("Please enter a valid amount.");
+    }
+    System.out.println(acc.getBalance());
   }
 }
